0e25215f5f6be1ca440743e101d2856a6911a7c0
Snake <per.mortis.non.lates@gmail.com>
2023-01-30 23:30:58 +0500

    comleted

---
+++ TaskWithStar/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите функцию, которая принимает одно число - высоту елочки и рисует ее в консоли звездочками. (Пример см. презентацию)
+
+Console.Clear();
+Console.WriteLine("Какой высоты будет ёлочка? Введите цифру: ");
+int height = int.Parse(Console.ReadLine()!);
+Fir(height, ' ', '*');
+
+void Fir(int level, char gap, char sim)
+{
+    int occ = 1;
+    for (int i = 0; i < level; i++ )
+    {
+        int emp = level - 1;
+        emp -= i;
+        string space = string.Empty;
+        string star = string.Empty;
+        while (emp != 0)
+        {
+            space += gap;
+            emp--;
+        }
+        for (int count = 1; count <= occ; count++)
+        {
+            star += sim;
+        }
+        Console.WriteLine($"{space}{star}");
+        occ += 2;
+    }
+}
